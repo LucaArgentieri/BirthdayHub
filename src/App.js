2c7261136b68d701ff153992e792746513ab0500
Luca Argentieri <Lucaargentieri-98@hotmail.it>
2021-11-23 10:23:03 +0100

    Update onesignal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
+import OneSignal from "react-onesignal";
 import BirthdayHub from "./BirthdayHub";
 
 function App() {
+  useEffect(() => {
+    OneSignal.init({
+      appId: process.env.REACT_APP_ONESIGNAL_API_KEY,
+    });
+  }, []);
   return (
     <div className="App">
       <BirthdayHub />
